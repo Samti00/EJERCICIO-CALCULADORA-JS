47a7795208ad9b9c683125f25fae359a64a485cd
Samti Alarcon <samti.alarcon@hotmail.com>
2024-08-20 02:07:29 -0500

    ejercicio de calculadora

---
+++ script.js
@@ -0,0 +1,12 @@
+//AGREGA A LA PANTALLA EL VALOR DEL BOTON DE LA CALCULADORA
+function agregar(valor) {
+  document.getElementById("pantalla").value += valor;
+}
+function borrar() {
+    document.getElementById("pantalla").value = '';
+}
+function calcular(){
+    const valorPantalla=document.getElementById("pantalla").value;
+    const resultado=eval(valorPantalla);
+    document.getElementById("pantalla").value = resultado;
+}
